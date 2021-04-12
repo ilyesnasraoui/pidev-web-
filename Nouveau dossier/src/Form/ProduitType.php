a0583c6fc689dff629deed6adba210a41b1a9fd0
Sururrrr <sourour.cherif@esprit.tn>
2021-04-12 21:10:27 +0200

    ee
    yy

---
+++ Nouveau dossier/src/Form/ProduitType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('idCategorie')
+            ->add('couleur')
+            ->add('nomProduit')
+            ->add('prix')
+            ->add('image',FileType::class, array('required' => true, 'label' => 'Image de la produit','data_class'=>null))
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}

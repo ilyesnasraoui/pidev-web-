32f99be24d2fe35e9416778baa4a51e91ecae472
ilyesnasraoui <mohamedilyes.nasraoui@esprit.tn>
2021-05-19 12:29:11 +0100

    bla

--- Nouveau dossier/src/Entity/Evenement.php
+++ Nouveau dossier/src/Entity/Evenement.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+
 
 
 /**
@@ -19,6 +22,7 @@ class Evenement
      * @ORM\Column(name="id_evenement", type="integer", nullable=false)
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @Groups("post:read")
      */
     private $idEvenement;
 
@@ -26,6 +30,7 @@ class Evenement
      * @var int
      *
      * @ORM\Column(name="id_cat_evenement", type="integer", nullable=false)
+     * @Groups("post:read")
      */
     private $idCatEvenement;
 
@@ -33,6 +38,7 @@ class Evenement
      * @var int
      *
      * @ORM\Column(name="id_user", type="integer", nullable=false)
+     * @Groups("post:read")
      */
     private $idUser;
 
@@ -40,6 +46,7 @@ class Evenement
      * @var string
      *
      * @ORM\Column(name="nom_evenement", type="string", length=100, nullable=false)
+     * @Groups("post:read")
      */
     private $nomEvenement;
 
@@ -47,12 +54,14 @@ class Evenement
      * @var string
      *
      * @ORM\Column(name="date_evenement", type="string", length=100, nullable=false)
+     * @Groups("post:read")
      */
     private $dateEvenement;
 
     /**
      * @var int
      * @ORM\Column(name="duree_evenement", type="integer", nullable=false)
+     * @Groups("post:read")
      */
     private $dureeEvenement;
 
@@ -60,6 +69,7 @@ class Evenement
      * @var string
      *
      * @ORM\Column(name="image_evnement", type="string", length=200, nullable=false)
+     * @Groups("post:read")
      */
     private $imageEvnement;
 
@@ -67,6 +77,7 @@ class Evenement
      * @var string
      *
      * @ORM\Column(name="description", type="string", length=100, nullable=false)
+     * @Groups("post:read")
      */
     private $description;
 
@@ -74,6 +85,7 @@ class Evenement
      * @var int|null
      *
      * @ORM\Column(name="validate", type="integer", nullable=true)
+     * @Groups("post:read")
      */
     private $validate;
 
